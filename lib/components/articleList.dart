a60a16c41dd8b4fe1b5e3990ba5fe6d045fc5f7d
AlulCode45 <alulgans70@gmail.com>
2024-12-31 10:13:25 +0700

    feat: add article

---
+++ lib/components/articleList.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class Articlelist extends StatelessWidget {
+  final String title;
+  final String createdAt;
+  final String? thumbnail;
+
+  const Articlelist(
+      {super.key,
+      required this.title,
+      required this.createdAt,
+      this.thumbnail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        spacing: 20,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey,
+              ),
+              height: 150,
+              clipBehavior: Clip.hardEdge,
+              // Memastikan isi mengikuti border radius
+              child: Image.network(
+                thumbnail!,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child; // Gambar selesai dimuat
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null, // Progress jika diketahui
+                    ),
+                  );
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Center(
+                    child:
+                        Icon(Icons.error, color: Colors.red), // Icon jika error
+                  );
+                },
+              ),
+            ),
+          ),
+          Flexible(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                spacing: 5,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                    child: Text(
+                      '#coding',
+                      style: TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      '@acode',
+                      style: TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(createdAt)
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,30 @@
+import 'package:a_learning2/components/articleList.dart';
 import 'package:a_learning2/components/searchForm.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  Home({super.key});
+
   final TextEditingController _searchController = TextEditingController();
 
-  Home({super.key});
+  final List<Map<String, String>> articles = [
+    {
+      'title': 'Exploring the World of Flutter Widgets',
+      'createdAt': 'Jan 12 - 8 min read',
+      'thumbnail': 'https://www.computerhope.com/jargon/p/program.png'
+    },
+    {
+      'title': 'How to Build a Responsive UI in Flutter',
+      'createdAt': 'Jan 15 - 5 min read',
+      'thumbnail': 'https://cdn0-production-images-kly.akamaized.net/bzky3jYn9h2VeCewid1oc-fJJ-s=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2962989/original/075702800_1573353762-20190326113607-Digital-Options-With-Olymp-Trade-Online-Trading-Made-Simple-2-4000pxW-X-2670pxH.jpeg'
+    },
+    {
+      'title': 'State Management Simplified with Provider',
+      'createdAt': 'Jan 18 - 10 min read',
+      'thumbnail': 'https://codingweek.org/wp-content/uploads/2019/08/christopher-robin-ebbinghaus-pgSkeh0yl8o-unsplash.jpg'
+    },
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +32,25 @@ class Home extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          children: [Searchform(searchController: _searchController)],
+          children: [
+            Searchform(searchController: _searchController),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  final article = articles[index];
+                  return Articlelist(
+                    title: article['title']!,
+                    createdAt: article['createdAt']!,
+                    thumbnail: article['thumbnail'],
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
