761f3d4b5f98a9b7cb5a354afd89174941ac03d8
AlulCode45 <alulgans70@gmail.com>
2024-12-31 11:58:03 +0700

    feat: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:a_learning2/components/appBarComponent.dart';
 import 'package:a_learning2/components/drawerComponent.dart';
 import 'package:a_learning2/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
